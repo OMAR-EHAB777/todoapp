da1f6171582767a5cb22d32090e842f550f36b6f
omar <ramomixes@hotmail.com>
2020-10-28 12:15:22 +0200

    curl : The URI prefix is not recognized.

--- superlists/settings.py
+++ superlists/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!p@_6npgb+r5l$#chhvp-=#k#8ht@gpl9nutsf3ep50f$9srl3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
